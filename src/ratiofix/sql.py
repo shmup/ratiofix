88827073de905633e368cda5d6ef2da37c9c99b1
Jared Miller <jtmiller@gmail.com>
2014-04-09 09:09:22 -0400

    didn't need to drop shit

--- src/ratiofix/sql.py
+++ src/ratiofix/sql.py
@@ -4,10 +4,7 @@ db = sqlite3.connect('torrents.db')
 
 cursor = db.cursor()
 
-cursor.execute('DROP TABLE IF EXISTS torrents;')
-db.commit()
-
-cursor.execute('CREATE TABLE torrents(id INTEGER);')
+cursor.execute('CREATE TABLE IF NOT EXISTS torrents(id INTEGER);')
 db.commit()
 
 cursor.execute('CREATE UNIQUE INDEX torrents_id ON torrents(id);')
